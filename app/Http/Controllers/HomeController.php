1400ede157c26092f11f8b12cbb21dc49929288a
sem2602 <sem2602@gmail.com>
2022-09-28 14:33:43 +0300

    edit

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Settings;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -10,7 +11,39 @@ class HomeController extends Controller
 
     public function index()
     {
-        return view('home');
+
+        $AllOrders = [];
+
+        $promApis = Settings::where('service_id', 2)->get();
+
+        // Prom Orders
+        foreach ($promApis as $api){
+
+            $prom = new Services\Prom($api->value);
+
+            $PromOrders = $prom->getPendingOrders();
+
+            if(!empty($PromOrders['orders'])){
+                foreach ($PromOrders['orders'] as $order){
+                    $order['api_id'] = $api->id;
+                    $AllOrders[] = $order;
+                }
+            }
+
+        }
+        //////////////////////////////////////////////////////
+
+
+
+        echo '<pre>';
+        var_dump($AllOrders);
+        exit;
+
+
+
+        return view('home', [
+            'orders' => $AllOrders
+        ]);
     }
 
 }
